80c906380624425533e00e6d5c11748ee3fdfda0
Aziz ALKAN <56042715+azzlkn@users.noreply.github.com>
2022-10-22 13:21:38 +0300

    fix: updates old personal information

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,41 @@ import React from 'react';
 
 function Index() {
   return (
-    <div className="container mx-auto px-3">
-      <h1 className="pt-3 font-body text-4xl font-semibold md:text-4xl lg:text-5xl">Merhaba, ben Aziz. 👋</h1>
-      <h2 className="pt-3 text-2xl">Balıkesir'de yaşayan bir yazılım geliştiriciyim.</h2>
-      <p className="pt-5 font-body text-xl font-light text-gray">
-        Yazılımı, yazmayı, konuşmayı ve yürüyüşü severim. Kurumsal müşterilere özel yazılım geliştirme hizmetleri sunan
-        Dada'nın kurucu ortağıyım. Ayrıca yarı zamanlı olarak fotoğraf çekiyor, kahve ve şarap tadıyorum.
+    <div className='container mx-auto px-3'>
+      <h1 className='pt-3 font-body text-4xl font-semibold md:text-4xl lg:text-5xl'>
+        Merhaba, ben Aziz. 👋
+      </h1>
+      <h2 className='pt-3 text-2xl'>
+        Balıkesir'de yaşayan bir yazılım geliştiricisiyim.
+      </h2>
+      <p className='pt-5 font-body text-xl font-light text-gray'>
+        Yazılımı, yazmayı, konuşmayı ve yürüyüşü severim.{' '}
+        <p className='pt-5 font-body text-xl font-light text-gray'>
+          <a
+            href='https://www.teyit.org'
+            target='_blank'
+            className='text-light'
+          >
+            Teyit
+          </a>{' '}
+          isimli bilgi ekosistemi aktörlerinde eleştirel düşünme alışkanlığını
+          ortak değer kılmayı amaçlayan bir doğrulama organizasyonunda yazılım
+          geliştiricisi olarak çalışıyorum.
+        </p>{' '}
+        <p className='pt-5 font-body text-xl font-light text-gray'>
+          {' '}
+          Ayrıca yarı zamanlı olarak fotoğraf çekiyor, kahve ve şarap tadıyorum.
+        </p>
       </p>
-      <p className="pt-5 font-body text-xl font-light text-gray">
-        Yeniden aktif olmaya karar verdiğim Youtube kanalımda ve blog yazılarımda ise yazılım tecrübelerimi ve
-        bilgilerimi aktarıyorum.
+      <p className='pt-5 font-body text-xl font-light text-gray'>
+        Bunların yanında Youtube kanalımda ve blog yazılarımda ise yazılım
+        tecrübelerimi ve bilgilerimi aktarıyorum.
       </p>
 
-      <h2 className="pt-5 text-xl">
-        Ayrıca haftalık olarak yazılım, teknoloji, kahve ve benzeri konularda oluşturduğum bültenim{' '}
-        <a href="/luminar" className="text-light">
+      <h2 className='pt-5 text-xl'>
+        Aylık olarak yazılım, teknoloji, kahve, kitap ve benzeri konularda
+        oluşturduğum bültenim{' '}
+        <a href='/luminar' className='text-light'>
           "Luminar"
         </a>
         ı takip etmeni öneririm.
--- pages/luminar.tsx
+++ pages/luminar.tsx
@@ -3,49 +3,58 @@ import React from 'react';
 
 function Luminar() {
   return (
-    <div className="container mx-auto px-3">
+    <div className='container mx-auto px-3'>
       <Head>
         <title>Luminar Bülten</title>
       </Head>
-      <h1 className="pt-3 text-3xl">Luminar Bülten</h1>
+      <h1 className='pt-3 text-3xl'>Luminar Bülten</h1>
 
-      <div className="pt-3">
-        <p className="font-body font-light">
-          Hafta sona ererken o hafta yaşanan yazılım, teknoloji, kahve ve daha birçok konuda özenle hazırlanmış bir
-          pazar bültenidir. Mail adresini kaydederek sen de her pazar saat 11:00'de posta kutunda güzel içeriklere
-          ulaşabilirsin.
+      <div className='pt-3'>
+        <p className='font-body font-light'>
+          Ay sona ererken o ay yaşanan yazılım, teknoloji, kahve, kitap ve daha
+          birçok konuda özenle hazırlanmış bir ay sonu bültenidir. Mail adresini
+          kaydederek sen de her ayın son pazarı saat 11:00'de posta kutunda
+          güzel içeriklere ulaşabilirsin.
         </p>
       </div>
 
       <form
-        action="https://www.getrevue.co/profile/luminar/add_subscriber"
-        method="post"
-        id="revue-form"
-        name="revue-form"
-        target="_blank"
+        action='https://www.getrevue.co/profile/luminar/add_subscriber'
+        method='post'
+        id='revue-form'
+        name='revue-form'
+        target='_blank'
       >
-        <div className="flex mt-3">
-          <div className="w-full sm:mr-3 sm:w-2/3">
+        <div className='flex mt-3'>
+          <div className='w-full sm:mr-3 sm:w-2/3'>
             <input
-              type="email"
-              name="member[email]"
-              id="member_email"
-              placeholder="Bülteni almak istediğiniz mail adresi"
-              className="w-full border border-primary px-5 py-4 font-body font-light text-sub placeholder-sub transition-colors focus:border-sub focus:outline-none focus:ring-2 focus:ring-sub"
+              type='email'
+              name='member[email]'
+              id='member_email'
+              placeholder='Bülteni almak istediğiniz mail adresi'
+              className='w-full border border-primary px-5 py-4 font-body font-light text-sub placeholder-sub transition-colors focus:border-sub focus:outline-none focus:ring-2 focus:ring-sub'
             />
           </div>
-          <button className="w-1/3 bg-sub text-center font-body text-xl font-semibold text-white transition-colors hover:bg-primary sm:inline-block sm:text-2xl">
+          <button className='w-1/3 bg-sub text-center font-body text-xl font-semibold text-white transition-colors hover:bg-primary sm:inline-block sm:text-2xl'>
             Abone ol
           </button>
         </div>
       </form>
-      <p className="font-body text-sm font-light text-gray mt-2">
+      <p className='font-body text-sm font-light text-gray mt-2'>
         *By subscribing, you agree with Revue’s{' '}
-        <a className="text-light" target="_blank" href="https://www.getrevue.co/terms">
+        <a
+          className='text-light'
+          target='_blank'
+          href='https://www.getrevue.co/terms'
+        >
           Terms of Service
         </a>{' '}
         and{' '}
-        <a className="text-light" target="_blank" href="https://www.getrevue.co/privacy">
+        <a
+          className='text-light'
+          target='_blank'
+          href='https://www.getrevue.co/privacy'
+        >
           Privacy Policy
         </a>
         .
